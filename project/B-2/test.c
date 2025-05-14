6c57fc4d8304dc149b9c403fcb962fa15ad78f18
Oneposhii <ipposhi-y01@asoalpha-net.com>
2025-05-14 15:10:48 +0900

    a

--- project/B-2/test.c
+++ project/B-2/test.c
@@ -1,34 +1,24 @@
 ﻿#include <stdio.h>
-#include "sqlite3.h"
+#include <sqlite3.h>
+#include <register.h>
+#include <update.h>
+#include <string.h>
 
-int main()
-{
-    sqlite3* db;
-    char* err_msg = 0;
+int main() {
+    int select;
+    printf("1: 新規登録\n 2: 更新\n ");
+    (void)scanf("%d", &select);
 
-    // データベースを開く（なければ作成される）
-    if (sqlite3_open("mydb.db", &db) != SQLITE_OK)
-    {
-        printf("Cannot open DB: %s\n", sqlite3_errmsg(db));
-        return 1;
+    if (select == 1) {
+        register_infomation();
+    }
+    else if (select == 2) {
+        update_infomation();
+    }
+    else {
+        printf("やり直し\n");
     }
 
-    // テーブル作成SQL
-    const char* sql = "CREATE TABLE Students ("
-                      "student_id INTEGER PRIMARY KEY,"
-                      "student_name TEXT NOT NULL);";
-
-    // 実行
-    if (sqlite3_exec(db, sql, 0, 0, &err_msg) != SQLITE_OK)
-    {
-        printf("SQL error: %s\n", err_msg);
-        sqlite3_free(err_msg);
-    }
-    else
-    {
-        printf("Table created successfully\n");
-    }
-
-    sqlite3_close(db);
     return 0;
 }
+
